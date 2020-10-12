1b23e7c241e6f3bb1565c68eb7a16268df391d01
Ronan <liruonan@hotmail.com>
2020-10-12 21:56:57 +0800

    update(utils.index): 添加一个test2()

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,11 +2,16 @@ import * as XLSX from 'xlsx'
 
 export const excelToJson = () => {
     console.warn('@/utils/index.excelToJson is used')
-    return XLSX
+    return XLSX + 'excelToJson';
 }
 
 
 export const test = () => {
     console.warn('@/utils/index.test is used')
     return 'test'
+}
+
+export const test2 = () => {
+    console.warn('@/utils/index.test2 is used')
+    return 'test2'
 }
